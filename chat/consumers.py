9ac0ef18eda57f51b915e6ae3786ffdd425f86b1
AjmalPonneth <ponnethajmal@gmail.com>
2021-11-01 14:15:35 +0530

    connected websocket

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,24 +1,46 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+
+        # join room
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'tester',
-            }
-        )
+        await self.accept()
 
-    async def disconnect(self, code):
+    async def disconnect(self, close_code):
+        # leave room
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data.get('message')
+        username = data.get('username')
+        room = data.get('room')
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username
+        }))
--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,5 @@
-from django.urls import re_path
+from django.urls import path
 from . import consumers
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatRoomConsumer)
+    path('ws/<str:room_name>/',  consumers.ChatRoomConsumer.as_asgi())
 ]
